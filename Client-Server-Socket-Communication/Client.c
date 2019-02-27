92e085b85d96852d88c885fcfa8df94616b69d93
rgabeflores <rgabeflores@gmail.com>
2019-02-26 21:32:22 -0800

    Fixing errors

---
+++ Client-Server-Socket-Communication/Client.c
@@ -0,0 +1,55 @@
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+
+#define PORT 8080 
+
+/**
+	This is a client for sending TCP messages to a server.
+*/
+
+int main(int argc, char const *argv[]) 
+{ 
+	struct sockaddr_in address; 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+
+	char message[1024]; // Used for message to send to server
+	char buffer[2048] = {0}; // Used for response message from server
+	
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	}
+	printf("Type something to send a message:\n");
+	while(1){
+		if(fgets(message,100,stdin)){
+			send(sock , message , strlen(message) , 0 );
+			printf("You: %s\n", message);
+		}
+		if(read( sock , buffer, 2048)) printf("Server: %s\n",buffer );
+	}
+	return 0; 
+}
