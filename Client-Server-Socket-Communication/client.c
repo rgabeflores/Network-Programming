eb6016a16e525373b3d97b1e75fe87d5adfbd4c6
rgabeflores <rgabeflores@gmail.com>
2019-02-26 21:21:59 -0800

    Comments and refactoring

--- Client-Server-Socket-Communication/client.c
+++ Client-Server-Socket-Communication/client.c
@@ -6,25 +6,25 @@
 #include <string.h> 
 #define PORT 8080 
 
-
-//This function checks if input1 matches the exitChar string.
-//Return: 0 if message matches exitChar
-int exitFunc(char* input1, char* input2){
-int c = -4;
-	for(int i = 0;i<4;i++){
-		if(input1[i] == input2[i]){
-		c++;
-	}
-}
-return c;
-}
-
 /**
 	This is a client for sending TCP messages to a server.
 */
 
-int main(int argc, char const *argv[]) 
-{ 
+/**
+	This function checks if input1 matches the exitChar string.
+	Return: 0 if message matches exitChar
+*/
+int exitFunc(char* input1, char* input2){
+	int c = -4;
+		for(int i = 0;i<4;i++){
+			if(input1[i] == input2[i]){
+			c++;
+		}
+	}
+	return c;
+}
+
+int main(int argc, char const *argv[]) { 
 	struct sockaddr_in address; 
 	int sock = 0, valread; 
 	struct sockaddr_in serv_addr; 
@@ -34,6 +34,8 @@ int main(int argc, char const *argv[])
 	char exitChar[1024] = "exit"; // Used to check input message
 	char message[1024]; // Used for message to send to server
 	char buffer[2048] = {0}; // Used for response message from server
+
+
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{ 
 		printf("\n Socket creation error \n"); 
@@ -59,20 +61,18 @@ int main(int argc, char const *argv[])
 	}
 	while(exit == 0){ 
 
-	printf("Type 'exit' to exit");
-	printf("\n");
+		printf("Type 'exit' to exit");
+		printf("\n");
 
-	if(fgets(message,100,stdin)){
-		if(exitFunc(message,exitChar) == 0){ //Client closes after exit string is inputted
-		printf("Exiting... \n");
-		return 0;
+		if(fgets(message,100,stdin)){
+			if(exitFunc(message,exitChar) == 0){ //Client closes after exit string is inputted
+				printf("Exiting... \n");
+				return 0;
+			}
+			send(sock , message , strlen(message) , 0 );
+			printf("You: %s\n", message);
 		}
-		send(sock , message , strlen(message) , 0 );
-		printf("You: %s\n", message);
-		}
-	if(read( sock , buffer, 2048)) printf("Server: %s\n",buffer );
-
-	
+		if(read( sock , buffer, 2048)) printf("Server: %s\n",buffer );
 	}
 	return 0; 
 }
