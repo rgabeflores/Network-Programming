845f70c285c00d88d521088920ec30d896d4a9eb
Hayden Matheus <haydenh2o@yahoo.com>
2019-03-19 20:58:43 -0700

    Add files via upload

--- CORBA-Distributed-System/CORBA/cpp/MyExampleInterface_impl.cpp
+++ CORBA-Distributed-System/CORBA/cpp/MyExampleInterface_impl.cpp
@@ -10,3 +10,45 @@ char * MyExampleInterface_impl::send_message(const char * message)
 	strncpy(server, "Message from C++ (omniORB) server", 42);
 	return server;
 }
+
+char * MyExampleInterface_impl::newQuestion(const char * question,const char * answer)
+{
+	cout << "C++ (omniORB) server: " << "Enter a question" << endl;
+	char * server = CORBA::string_alloc(42);
+	strncpy(server, question, 42);
+	return server;
+}
+
+char * MyExampleInterface_impl::askQuestion()
+{
+	cout << "C++ (omniORB) server: " << "Enter a question" << endl;
+	
+
+}
+
+void MyExampleInterface_impl::removeQuestion()
+{
+	int input = 0;
+	displayAllQuestions();
+	cout << "Enter the question number of the question to remove" << endl;
+	cin>>input;
+	input = input - 1;
+	questions.erase(input);
+	cout << "Question at index: " << input << " deleted."<<endl;
+}
+
+void MyExampleInterface_impl::displayAllQuestions()
+{
+	for(int i = 0; i<questions.size();i++){
+		cout << "Question " << i << ": " << questions[i]<< endl;
+	}
+}
+
+void MyExampleInterface_impl::displayAll()
+{
+	for(int i = 0; i<questions.size();i++){
+		cout << "Question " << i << ": " << questions[i]<< endl;
+		cout << "Answer " << i << ": " << answers[i]<< endl;
+	}
+}
+
--- CORBA-Distributed-System/CORBA/cpp/MyExampleInterface_impl.h
+++ CORBA-Distributed-System/CORBA/cpp/MyExampleInterface_impl.h
@@ -2,11 +2,19 @@
 #define __MY_EXAMPLE_INTERFACE_IMPL_H__
 
 #include "example.hh"
+#include <string.h>
+#include <vector>
+
 
 class MyExampleInterface_impl : public POA_ExampleInterface
 {
 	public:
+		vector<string> questions;
+		vector<string> answers;
 		virtual char * send_message(const char * message);
+		virtual char * newQuestion(const char * question,const char * answer);
+		virtual void displayAllQuestions();		
+		virtual void displayAll();
 };
 
 #endif // __MY_EXAMPLE_INTERFACE_IMPL_H__
