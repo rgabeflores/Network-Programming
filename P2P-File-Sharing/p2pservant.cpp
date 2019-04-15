b3458e10c6a511684f7c383f55ba3e9dc1ae3fac
rgabeflores <rgabeflores@gmail.com>
2019-04-15 01:11:31 -0700

    Initial Commit

---
+++ P2P-File-Sharing/p2pservant.cpp
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+
+int main (int argc, char* argv[])
+{
+
+	return 0;
+}
