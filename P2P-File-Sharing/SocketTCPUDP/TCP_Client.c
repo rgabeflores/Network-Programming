cba44c9b284973126a99f625c9810770dfb6b87f
Hayden Matheus <haydenh2o@yahoo.com>
2019-04-22 11:36:32 -0700

    added multithreaded TCP+UDP calls

--- P2P-File-Sharing/SocketTCPUDP/TCP_Client.c
+++ P2P-File-Sharing/SocketTCPUDP/TCP_Client.c
@@ -5,16 +5,22 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <sys/types.h> 
+#include <pthread.h>
+#include <unistd.h>
+#define NUM_THREADS 2
 #define PORT 5000 
 #define MAXLINE 1024 
-int main() 
-{ 
+
+void TCP_CALL()
+{
 	int sockfd; 
 	char buffer[MAXLINE]; 
 	char* message = "Hello Server"; 
-	struct sockaddr_in servaddr; 
 
-	int n, len; 
+	struct sockaddr_in servaddr;
+
+
+
 	// Creating socket file descriptor 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
 		printf("socket creation failed"); 
@@ -23,7 +29,6 @@ int main()
 
 	memset(&servaddr, 0, sizeof(servaddr)); 
 
-	// Filling server information 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
@@ -39,5 +44,64 @@ int main()
 	printf("Message from server: "); 
 	read(sockfd, buffer, sizeof(buffer)); 
 	puts(buffer); 
-	close(sockfd); 
+	close(sockfd);
+}
+
+void UDP_CALL(){
+	
+
+	int sockfd;
+	int sockfd_UDP;
+	char UDPbuffer[MAXLINE]; 
+	struct sockaddr_in servaddr;
+
+	if ((sockfd_UDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+		printf("UDP socket creation failed"); 
+		exit(0); 
+	} 
+
+	char* message1 = "Hello Server UDP"; 
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+
+	// Filling server information 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+
+	sendto(sockfd_UDP, (const char*)message1, strlen(message1), 
+		0, (const struct sockaddr*)&servaddr, 
+		sizeof(servaddr)); 
+
+	for(;;){
+		sleep(5);		
+		sendto(sockfd_UDP, (const char*)message1, strlen(message1), 
+		0, (const struct sockaddr*)&servaddr, 
+		sizeof(servaddr)); 
+	}
+}
+
+int main() 
+{ 
+
+	int rc;
+	int i;
+	
+	
+	pthread_t thread_array[NUM_THREADS];
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	rc = pthread_create(&thread_array[0], NULL, TCP_CALL, (void*) NULL);
+	rc = pthread_create(&thread_array[1], NULL, UDP_CALL, (void*) NULL);
+	
+	for(int i = 0;i<2;i++){
+		pthread_join(thread_array[i],NULL);
+	}
+
+	
+	
+	 
 } 
