ea70a8f3d35d0de0ff64f5fe24ffda0940ae69a4
rgabeflores <rgabeflores@gmail.com>
2019-05-01 02:13:08 -0700

    Refactored and removed warnings

--- P2P-File-Sharing/SocketTCPUDP/TCP_Client.c
+++ P2P-File-Sharing/SocketTCPUDP/TCP_Client.c
@@ -11,71 +11,76 @@
 #define PORT 5000 
 #define MAXLINE 1024 
 
-void TCP_CALL()
-{
-	int sockfd; 
+void * TCP_CALL(){
+
+	int sockfd_TCP, rv; 
 	char buffer[MAXLINE]; 
 	char* message = "Hello Server"; 
 
 	struct sockaddr_in servaddr;
 
-
-
 	// Creating socket file descriptor 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+	if ((sockfd_TCP = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
 		printf("socket creation failed"); 
 		exit(0); 
 	} 
 
 	memset(&servaddr, 0, sizeof(servaddr)); 
 
+	// Filling server address information
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
 
-	if (connect(sockfd, (struct sockaddr*)&servaddr, 
+	// Establish Connection to Destination TCP Port
+	if (connect(sockfd_TCP, (struct sockaddr*)&servaddr, 
 							sizeof(servaddr)) < 0) { 
 		printf("\n Error : Connect Failed \n"); 
 	} 
 
+	// Send Initial TCP Message
 	memset(buffer, 0, sizeof(buffer)); 
-	strcpy(buffer, "Hello Server"); 
-	write(sockfd, buffer, sizeof(buffer)); 
+	strcpy(buffer, message); 
+	rv = write(sockfd_TCP, buffer, sizeof(buffer));
+	if (rv < 0) printf("\nThere was an error.\n");
+
+	// Receive Response from Server
 	printf("Message from server: "); 
-	read(sockfd, buffer, sizeof(buffer)); 
+	rv = read(sockfd_TCP, buffer, sizeof(buffer));
+
+	if (rv < 0) printf("\nThere was an error.\n");
 	puts(buffer); 
-	close(sockfd);
+	close(sockfd_TCP);
 }
 
-void UDP_CALL(){
-	
+void * UDP_CALL(){
 
-	int sockfd;
 	int sockfd_UDP;
-	char UDPbuffer[MAXLINE]; 
+	// char UDPbuffer[MAXLINE]; 
 	struct sockaddr_in servaddr;
+	char* message = "Hello Server UDP"; 
 
 	if ((sockfd_UDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
 		printf("UDP socket creation failed"); 
 		exit(0); 
 	} 
-
-	char* message1 = "Hello Server UDP"; 
 	
 	memset(&servaddr, 0, sizeof(servaddr)); 
 
-	// Filling server information 
+	// Filling server address information 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
 
-	sendto(sockfd_UDP, (const char*)message1, strlen(message1), 
+	// Send Initial UDP Message
+	sendto(sockfd_UDP, (const char*)message, strlen(message), 
 		0, (const struct sockaddr*)&servaddr, 
 		sizeof(servaddr)); 
 
+	// Send UDP Hello every 5 seconds
 	for(;;){
 		sleep(5);		
-		sendto(sockfd_UDP, (const char*)message1, strlen(message1), 
+		sendto(sockfd_UDP, (const char*)message, strlen(message), 
 		0, (const struct sockaddr*)&servaddr, 
 		sizeof(servaddr)); 
 	}
@@ -85,8 +90,6 @@ int main()
 { 
 
 	int rc;
-	int i;
-	
 	
 	pthread_t thread_array[NUM_THREADS];
 	pthread_attr_t attr;
@@ -95,13 +98,13 @@ int main()
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
 	rc = pthread_create(&thread_array[0], NULL, TCP_CALL, (void*) NULL);
+	if(rc != 0) printf("There was a problem.");
+
 	rc = pthread_create(&thread_array[1], NULL, UDP_CALL, (void*) NULL);
+	if(rc != 0) printf("There was a problem.");
 	
 	for(int i = 0;i<2;i++){
 		pthread_join(thread_array[i],NULL);
 	}
 
-	
-	
-	 
 } 
