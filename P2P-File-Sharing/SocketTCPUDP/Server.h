1319baafd5f27c14a15f7ae8466ae57ee75ef437
rgabeflores <rgabeflores@gmail.com>
2019-05-06 10:55:00 -0700

    Added methods

--- P2P-File-Sharing/SocketTCPUDP/Server.h
+++ P2P-File-Sharing/SocketTCPUDP/Server.h
@@ -1,6 +1,8 @@
 #ifndef SERVER
 #define SERVER
 
+#include <string.h>
+
 #define NUM_NODES 10
 
 struct entry {
@@ -11,6 +13,9 @@ struct entry {
 struct entry entries[NUM_NODES];
 int entry_count = 0;
 
+/**
+	Creates a new entry with the given filename and ID.
+*/
 struct entry createEntry(int ID, char * filename){
 	struct entry newEntry;
 	newEntry.id = ID;
@@ -18,11 +23,26 @@ struct entry createEntry(int ID, char * filename){
 	return newEntry;
 }
 
+/**
+	Saves a new entry with the given filename.
+*/
 int saveEntry(char * filename){
 	int x = entry_count;
 	entries[entry_count] = createEntry(entry_count, filename);
 	entry_count++;
 	return x;
+}
+
+/**
+	Returns the ID of the node with the filename. Returns -1 if filename not found.
+*/
+int findEntry(char * filename){
+	for(int i = 0; i < entry_count; i++){
+		if(strcmp(filename, entries[i].filename) == 0){
+			return i;
+		}
+	}
+	return -1;
 }
 
 
